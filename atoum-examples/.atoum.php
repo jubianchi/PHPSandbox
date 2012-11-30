1699a0e59f6d2363d45c4a9d7229ff0319711fcc
jubianchi <contact@jubianchi.fr>
2012-11-30 11:53:14 +0100

    Tweak configuration file

--- atoum-examples/.atoum.php
+++ atoum-examples/.atoum.php
@@ -1,3 +1,8 @@
 <?php
 
 $script->addTestAllDirectory(__DIR__ . '/tests/unit');
+
+$script->addDefaultReport();
+
+list($cliReport) = $script->getReports();
+$cliReport->addField(new atoum\report\fields\runner\atoum\logo());
