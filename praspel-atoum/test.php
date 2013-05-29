6ac0337ba2f8afaf06969b0665f79bd34a22b85c
jubianchi <contact@jubianchi.fr>
2013-05-29 19:28:04 +0200

    Add praspel-atoum sample

---
+++ praspel-atoum/test.php
@@ -0,0 +1,44 @@
+<?php
+namespace {
+    class SUT {
+        public function M1() {
+            return true;
+        }
+
+        public function M2() {
+            return 'bar';
+        }
+    }
+}
+
+namespace tests\units {
+    require_once __DIR__ . '/vendor/autoload.php';
+    require_once __DIR__ . '/vendor/hoa/core/Core.php';
+    require_once __DIR__ . '/vendor/atoum/atoum/scripts/runner.php';
+
+    from('Hoathis')
+        ->import('Atoum.Test.Asserter.Praspel')
+        ->import('Atoum.Test');
+
+    class SUT extends \Hoathis\Atoum\Test {
+
+        public function testM1n°1 ( ) {
+
+            $this
+                ->if($this->requires['x']->in = realdom()->boundinteger(0, 256))
+                ->and($this->requires['y']->in = realdom()->const('foo'))
+                ->and($this->ensures['\result']->in = realdom()->boolean())
+                ->and($this->praspel->verdict(new \SUT()))
+            ;
+        }
+
+        public function testM2n°1 ( ) {
+
+            $this
+                ->if($this->requires['x']->in = realdom()->const('foo'))
+                ->and($this->ensures['\result']->in = realdom()->const('bar'))
+                ->and($this->praspel->verdict(new \SUT()))
+            ;
+        }
+    }
+}
